e0f811bd309dccaad64af385c7ddcf7d3777fb4a
satyabrata28 <119559350+satyabrata28@users.noreply.github.com>
2022-12-01 14:16:48 +0530

    Add files via upload

---
+++ Perimeter_Rectangle.java
@@ -0,0 +1,10 @@
+public class Perimeter_Rectangle
+{
+public static void main(String args[])
+{
+int length=25;
+int breath=35;
+int Perimeter=2*(length+breath);
+System.out.println("Perimeter of a Rectangle is "+Perimeter);
+}
+}
